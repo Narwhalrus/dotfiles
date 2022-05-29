51079345edc265b0272eab6921cf2322c34469f3
Josh Riccobono <j.p.riccobono2@gmail.com>
2022-05-29 17:55:20 -0500

    Update install script for zplug

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+if [ ! command -v zsh &> /dev/null ]
+then
+    echo "Install zsh first please..."
+    exit 1
+fi
+
+ln -s .zshrc ~/.zshrc
+ln -s .zsh ~/.zsh
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-ln -s .zshrc ~/.zshrc
-ln -s .zsh ~/.zsh
+# Install zplug
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
